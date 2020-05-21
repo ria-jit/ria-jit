0ba0dfbf4d7af0aacc37e31b342236abb54da0b1
Simon Kammemeier <simon.kammermeier@tum.de>
2020-05-21 18:07:40 +0200

    Define enum containing register mnemonics.

--- src/util.h
+++ src/util.h
@@ -62,6 +62,14 @@ typedef enum {
     x30, x31,
     pc
 } t_risc_reg;
+
+typedef enum {
+    zero,
+    ra, sp, gp, tp, t0, t1, t2, s0, fp = s0, s1,
+    a0, a1, a2, a3, a4, a5, a6, a7, s2, s3,
+    s4, s5, s6, s7, s8, s9, s10, s11, t3, t4,
+    t5, t6
+} t_risc_reg_mnem;
 #define N_REG 33
 
 //register value type
