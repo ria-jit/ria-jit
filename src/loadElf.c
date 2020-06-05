a56a075e903efe65413a31d2e1a34c3b37c7e2e1
Simon Kammemeier <simon.kammermeier@tum.de>
2020-06-05 18:02:36 +0200

    Implement stack allocation and loading with values.
    Now mapIntoMemory returns a struct with metadata.

--- src/loadElf.c
+++ src/loadElf.c
@@ -21,7 +21,7 @@
 #define EF_RISCV_TSO 0x10
 #endif
 
-t_risc_addr mapIntoMemory(char *filePath) {
+t_risc_elf_map_result mapIntoMemory(const char *filePath) {
     printf("Reading %s...\n", filePath);
 
     //get the file descriptor
@@ -45,7 +45,8 @@ t_risc_addr mapIntoMemory(char *filePath) {
     Elf64_Off ph_offset = header->e_phoff;
     Elf64_Phdr *segments = (Elf64_Phdr *) (exec + ph_offset);
     Elf64_Word flags = header->e_flags;
-
+    Elf64_Half phentsize = header->e_phentsize;
+    Elf64_Addr entry = header->e_entry;
     bool incompatible = false;
 
     if (flags & EF_RISCV_RVC) {
@@ -75,7 +76,7 @@ t_risc_addr mapIntoMemory(char *filePath) {
     if (incompatible) {
         return INVALID_ELF_MAP;
     }
-
+    t_risc_addr load_addr = 0;
     for(int i = 0; i < ph_count; i++) {
         Elf64_Phdr *segment = segments + i;
         switch(segment->p_type) {
@@ -90,6 +91,9 @@ t_risc_addr mapIntoMemory(char *filePath) {
                 if ((vaddr + memory_size) > TRANSLATOR_BASE) {
                     printf("Bad. This segment wants to be in the translators memory region");
                     return INVALID_ELF_MAP;
+                }
+                if (!load_addr) {
+                    load_addr = vaddr - load_offset;
                 }
                 //Copy flags over
                 int prot = 0;
@@ -108,7 +112,7 @@ t_risc_addr mapIntoMemory(char *filePath) {
                              load_offset);
                 //Failed means that we couldn't get enough memory at the correct address
                 if (BAD_ADDR(segment_in_memory)) {
-                    printf("Could not map segment %i because error %lu", i, (uintptr_t) segment_in_memory);
+                    printf("Could not map segment %i because error %li", i, -(intptr_t) segment_in_memory);
                     return INVALID_ELF_MAP;
                 }
                 //Check in case MAP_FIXED_NOREPLACE is not supported on that kernel version.
@@ -130,17 +134,15 @@ t_risc_addr mapIntoMemory(char *filePath) {
 
         }
     }
-
+    t_risc_addr phdr = load_addr + ph_offset;
     //Close and unmap the elf file
     munmap(exec, size);
     close(fd);
 
-
-    //Entry address for start of translation.
-    return header->e_entry;
+    return (t_risc_elf_map_result) {true, entry, phdr, ph_count, phentsize};
 }
 
-t_risc_addr createStack() {
+t_risc_addr allocateStack() {
     size_t stackSize = 8 * 1024 * 1024; //Default stack size
 
     struct rlimit rlimit;
@@ -159,13 +161,13 @@ t_risc_addr createStack() {
 
     //Failed means that we couldn't get enough memory at the correct address
     if (BAD_ADDR(bottomOfStack)) {
-        printf("Could not map stack because error %lu", (uintptr_t) (bottomOfStack));
-        return INVALID_ELF_MAP;
+        printf("Could not map stack because error %li", -(uintptr_t) (bottomOfStack));
+        return INVALID_STACK;
     }
     //Check in case MAP_FIXED_NOREPLACE is not supported on that kernel version.
     if ((uintptr_t) bottomOfStack != stackStart) {
         printf("Did not get the correct memory address for stack");
-        return INVALID_ELF_MAP;
+        return INVALID_STACK;
     }
 
     //Make guard protected
@@ -173,4 +175,75 @@ t_risc_addr createStack() {
 
     //Return top of stack
     return (t_risc_addr) bottomOfStack + guard + stackSize;
+}
+
+t_risc_addr copyArgsToStack(t_risc_addr stackPos, int guestArgc, char *guestArgv[], t_risc_elf_map_result
+mapInfo) {
+    ///Add auxv to guest stack
+    {
+        Elf64_auxv_t *stack = (Elf64_auxv_t *) stackPos;
+        *(--stack) = (Elf64_auxv_t) {AT_NULL};
+        *(--stack) = (Elf64_auxv_t) {AT_ENTRY, {mapInfo.entry}};
+        *(--stack) = (Elf64_auxv_t) {AT_PHDR, {mapInfo.entry}};
+        *(--stack) = (Elf64_auxv_t) {AT_PHNUM, {mapInfo.ph_count}};
+        *(--stack) = (Elf64_auxv_t) {AT_PHENT, {mapInfo.ph_entsize}};
+        *(--stack) = (Elf64_auxv_t) {AT_UID, {getauxval(AT_UID)}};
+        *(--stack) = (Elf64_auxv_t) {AT_GID, {getauxval(AT_GID)}};
+        *(--stack) = (Elf64_auxv_t) {AT_EGID, {getauxval(AT_EGID)}};
+        *(--stack) = (Elf64_auxv_t) {AT_EUID, {getauxval(AT_EUID)}};
+        *(--stack) = (Elf64_auxv_t) {AT_CLKTCK, {getauxval(AT_CLKTCK)}};
+        *(--stack) = (Elf64_auxv_t) {AT_RANDOM, {getauxval(AT_RANDOM)}}; //TODO Copy/Generate new one?
+        *(--stack) = (Elf64_auxv_t) {AT_SECURE, {0}};
+        *(--stack) = (Elf64_auxv_t) {AT_PAGESZ, {4096}};
+        *(--stack) = (Elf64_auxv_t) {AT_HWCAP, {0}}; //Seems to not be defined for RISCV
+        *(--stack) = (Elf64_auxv_t) {AT_HWCAP2, {0}}; //Seems to not be defined for RISCV
+//        *(--stack) = (Elf64_auxv_t) {AT_PLATFORM, {0}}; //Seems to not be defined for RISCV
+        *(--stack) = (Elf64_auxv_t) {AT_EXECFN, {(uintptr_t) guestArgv[0]}};
+        stackPos = (t_risc_addr) stack;
+    }
+    ///Copy envp to guest stack
+    {
+        int envc = 0;
+        for(; environ[envc]; ++envc);
+        char **stack = (char **) stackPos;
+        //Zero terminated so environ[envc] will be zero and also needs to be copied
+        for(int i = envc; i >= 0; i--) {
+            *(--stack) = environ[i];
+        }
+        for(int i = 0; stack[i] || environ[i]; ++i) {
+            if (stack[i] != environ[i]) {
+                printf("Difference in envp %p and environ %p\n", stack[i], environ[i]);
+            }
+        }
+        stackPos = (t_risc_addr) stack;
+    }
+    ///Copy guestArgv to guest stack
+    {
+        const char **stack = (const char **) stackPos;
+        //Zero terminated so guestArgv[guestArgc] will be zero and also needs to be copied
+        for(int i = guestArgc; i >= 0; i--) {
+            *(--stack) = guestArgv[i];
+        }
+        stackPos = (t_risc_addr) stack;
+    }
+    ///Copy guestArgc to guest stack
+    {
+        int *stack = (int *) stackPos;
+        *(--stack) = guestArgc;
+        stackPos = (t_risc_addr) stack;
+    }
+
+    return (t_risc_addr) stackPos;
+}
+
+
+t_risc_addr createStack(int guestArgc, char **guestArgv, t_risc_elf_map_result mapInfo) {
+    t_risc_addr stack = allocateStack();
+    if (!stack) {
+        return INVALID_STACK;
+    }
+    t_risc_addr argsToStack = copyArgsToStack(stack, guestArgc, guestArgv, mapInfo);
+    ///Stack pointer always needs to be 16-Byte aligned per ABI convention
+    return ALIGN_DOWN(argsToStack, 16u);
+
 }
--- src/loadElf.h
+++ src/loadElf.h
@@ -6,13 +6,33 @@
 #define DYNAMICBINARYTRANSLATORRISCV64_X86_64_LOADELF_H
 
 
-#define INVALID_ELF_MAP 0
+#define INVALID_ELF_MAP (t_risc_elf_map_result){.valid = false}
+#define INVALID_STACK 0
+/**
+ * Struct containing map metadata. If valid is false the other values are undefined.
+ */
+typedef struct {
+    bool valid;
+    t_risc_addr entry;
+    t_risc_addr phdr;
+    Elf64_Half ph_count;
+    Elf64_Half ph_entsize;
+} t_risc_elf_map_result;
 
 /**
  * Maps all LOAD segments of the ELF file at the given path into the correct memory regions.
- * @param filePath the path to the ELF file
- * @return the entry address of the ELF or INVALID_ELF_MAP if the mapping failed.
+ * @param filePath the path to the ELF file.
+ * @return the map result containing or INVALID_ELF_MAP if the mapping failed.
  */
-t_risc_addr mapIntoMemory(char *filePath);
+t_risc_elf_map_result mapIntoMemory(const char *filePath);
+
+/**
+ * Maps a stack for the program into memory and copies argc, argv, envp, and auxv onto it.
+ * @param guestArgc the number of arguments passed on to the guest program.
+ * @param guestArgv the array containing pointers to the argument strings passed on to the guest program.
+ * @param mapInfo the map result returned by the mapIntoMemory call.
+ * @return the 16-Byte aligned address of the start stack pointer.
+ */
+t_risc_addr createStack(int guestArgc, char *guestArgv[], t_risc_elf_map_result mapInfo);
 
 #endif //DYNAMICBINARYTRANSLATORRISCV64_X86_64_LOADELF_H
