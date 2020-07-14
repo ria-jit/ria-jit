d05a8cc1d01e3036729ce52ec4d2bdb767cac934
Simon Kammemeier <simon.kammermeier@tum.de>
2020-07-14 17:52:25 +0200

    Move translate_loadstore from ASMJit to faenc.

--- src/gen/instr/translate_loadstore.cpp
+++ src/gen/instr/translate_loadstore.cpp
@@ -5,10 +5,12 @@
 #include "translate_loadstore.hpp"
 #include "runtime/register.h"
 
-#define FIRST_REG x86::rax
-#define SECOND_REG x86::rdx
+#define FIRST_REG FE_AX
+#define SECOND_REG FE_DX
 
-using namespace asmjit;
+
+//shortcut for memory operands
+#define FE_MEM_ADDR(addr) FE_MEM(FE_IP, 0, 0, addr - (intptr_t) current)
 
 /**
  * Translate the LB instruction.
@@ -20,12 +22,16 @@ using namespace asmjit;
 void translate_LB(const t_risc_instr &instr, const register_info &r_info) {
     log_asm_out("Translate LB…\n");
 
-    if (r_info.mapped[instr.reg_dest] && r_info.mapped[instr.reg_src_1]) {
-        a->movsx(r_info.map[instr.reg_dest], x86::byte_ptr(r_info.map[instr.reg_src_1], instr.imm));
+    if (r_info.mapped[instr.reg_dest] || r_info.mapped[instr.reg_src_1]) {
+//        a->movsx(r_info.map[instr.reg_dest], x86::byte_ptr(r_info.map[instr.reg_src_1], instr.imm));
+        critical_not_yet_implemented("Register mapped instruction type unavailable\n");
     } else {
-        a->mov(SECOND_REG, x86::ptr(r_info.base + 8 * instr.reg_src_1));
+        /*a->mov(SECOND_REG, x86::ptr(r_info.base + 8 * instr.reg_src_1));
         a->movsx(FIRST_REG, x86::byte_ptr(SECOND_REG, instr.imm));
-        a->mov(x86::ptr(r_info.base + 8 * instr.reg_dest), FIRST_REG);
+        a->mov(x86::ptr(r_info.base + 8 * instr.reg_dest), FIRST_REG);*/
+        err |= fe_enc64(&current, FE_MOV64rm, SECOND_REG, FE_MEM_ADDR(r_info.base + 8 * instr.reg_src_1));
+        err |= fe_enc64(&current, FE_MOVSXr64m8, FIRST_REG, FE_MEM(SECOND_REG, 0, 0, instr.imm));
+        err |= fe_enc64(&current, FE_MOV64mr, FE_MEM_ADDR(r_info.base + 8 * instr.reg_dest), FIRST_REG);
     }
 }
 
@@ -39,12 +45,16 @@ void translate_LB(const t_risc_instr &instr, const register_info &r_info) {
 void translate_LH(const t_risc_instr &instr, const register_info &r_info) {
     log_asm_out("Translate LH…\n");
 
-    if (r_info.mapped[instr.reg_dest] && r_info.mapped[instr.reg_src_1]) {
-        a->movsx(r_info.map[instr.reg_dest], x86::word_ptr(r_info.map[instr.reg_src_1], instr.imm));
+    if (r_info.mapped[instr.reg_dest] || r_info.mapped[instr.reg_src_1]) {
+//        a->movsx(r_info.map[instr.reg_dest], x86::word_ptr(r_info.map[instr.reg_src_1], instr.imm));
+        critical_not_yet_implemented("Register mapped instruction type unavailable\n");
     } else {
-        a->mov(SECOND_REG, x86::ptr(r_info.base + 8 * instr.reg_src_1));
+        /*a->mov(SECOND_REG, x86::ptr(r_info.base + 8 * instr.reg_src_1));
         a->movsx(FIRST_REG, x86::word_ptr(SECOND_REG, instr.imm));
-        a->mov(x86::ptr(r_info.base + 8 * instr.reg_dest), FIRST_REG);
+        a->mov(x86::ptr(r_info.base + 8 * instr.reg_dest), FIRST_REG);*/
+        err |= fe_enc64(&current, FE_MOV64rm, SECOND_REG, FE_MEM_ADDR(r_info.base + 8 * instr.reg_src_1));
+        err |= fe_enc64(&current, FE_MOVSXr64m16, SECOND_REG, FE_MEM(SECOND_REG, 0, 0, instr.imm));
+        err |= fe_enc64(&current, FE_MOV64mr, FE_MEM_ADDR(r_info.base + 8 * instr.reg_dest), FIRST_REG);
     }
 }
 
@@ -58,12 +68,16 @@ void translate_LH(const t_risc_instr &instr, const register_info &r_info) {
 void translate_LW(const t_risc_instr &instr, const register_info &r_info) {
     log_asm_out("Translate LW…\n");
 
-    if (r_info.mapped[instr.reg_dest] && r_info.mapped[instr.reg_src_1]) {
-        a->movsxd(r_info.map[instr.reg_dest], x86::dword_ptr(r_info.map[instr.reg_src_1], instr.imm));
+    if (r_info.mapped[instr.reg_dest] || r_info.mapped[instr.reg_src_1]) {
+//        a->movsxd(r_info.map[instr.reg_dest], x86::dword_ptr(r_info.map[instr.reg_src_1], instr.imm));
+        critical_not_yet_implemented("Register mapped instruction type unavailable\n");
     } else {
-        a->mov(SECOND_REG, x86::ptr(r_info.base + 8 * instr.reg_src_1));
+        /*a->mov(SECOND_REG, x86::ptr(r_info.base + 8 * instr.reg_src_1));
         a->movsxd(FIRST_REG, x86::dword_ptr(SECOND_REG, instr.imm));
-        a->mov(x86::ptr(r_info.base + 8 * instr.reg_dest), FIRST_REG);
+        a->mov(x86::ptr(r_info.base + 8 * instr.reg_dest), FIRST_REG);*/
+        err |= fe_enc64(&current, FE_MOV64rm, SECOND_REG, FE_MEM_ADDR(r_info.base + 8 * instr.reg_src_1));
+        err |= fe_enc64(&current, FE_MOVSXr64m32, SECOND_REG, FE_MEM(SECOND_REG, 0, 0, instr.imm));
+        err |= fe_enc64(&current, FE_MOV64mr, FE_MEM_ADDR(r_info.base + 8 * instr.reg_dest), FIRST_REG);
     }
 }
 
@@ -77,12 +91,16 @@ void translate_LW(const t_risc_instr &instr, const register_info &r_info) {
 void translate_LBU(const t_risc_instr &instr, const register_info &r_info) {
     log_asm_out("Translate LBU…\n");
 
-    if (r_info.mapped[instr.reg_dest] && r_info.mapped[instr.reg_src_1]) {
-        a->movzx(r_info.map[instr.reg_dest], x86::byte_ptr(r_info.map[instr.reg_src_1], instr.imm));
+    if (r_info.mapped[instr.reg_dest] || r_info.mapped[instr.reg_src_1]) {
+//        a->movzx(r_info.map[instr.reg_dest], x86::byte_ptr(r_info.map[instr.reg_src_1], instr.imm));
+        critical_not_yet_implemented("Register mapped instruction type unavailable\n");
     } else {
-        a->mov(SECOND_REG, x86::ptr(r_info.base + 8 * instr.reg_src_1));
+        /*a->mov(SECOND_REG, x86::ptr(r_info.base + 8 * instr.reg_src_1));
         a->movzx(FIRST_REG, x86::byte_ptr(SECOND_REG, instr.imm));
-        a->mov(x86::ptr(r_info.base + 8 * instr.reg_dest), FIRST_REG);
+        a->mov(x86::ptr(r_info.base + 8 * instr.reg_dest), FIRST_REG);*/
+        err |= fe_enc64(&current, FE_MOV64rm, SECOND_REG, FE_MEM_ADDR(r_info.base + 8 * instr.reg_src_1));
+        err |= fe_enc64(&current, FE_MOVZXr64m8, FIRST_REG, FE_MEM(SECOND_REG, 0, 0, instr.imm));
+        err |= fe_enc64(&current, FE_MOV64mr, FE_MEM_ADDR(r_info.base + 8 * instr.reg_dest), FIRST_REG);
     }
 }
 
@@ -97,11 +115,15 @@ void translate_LHU(const t_risc_instr &instr, const register_info &r_info) {
     log_asm_out("Translate LHU…\n");
 
     if (r_info.mapped[instr.reg_dest] && r_info.mapped[instr.reg_src_1]) {
-        a->movzx(r_info.map[instr.reg_dest], x86::word_ptr(r_info.map[instr.reg_src_1], instr.imm));
+//        a->movzx(r_info.map[instr.reg_dest], x86::word_ptr(r_info.map[instr.reg_src_1], instr.imm));
+        critical_not_yet_implemented("Register mapped instruction type unavailable\n");
     } else {
-        a->mov(SECOND_REG, x86::ptr(r_info.base + 8 * instr.reg_src_1));
+        /*a->mov(SECOND_REG, x86::ptr(r_info.base + 8 * instr.reg_src_1));
         a->movzx(FIRST_REG, x86::word_ptr(SECOND_REG, instr.imm));
-        a->mov(x86::ptr(r_info.base + 8 * instr.reg_dest), FIRST_REG);
+        a->mov(x86::ptr(r_info.base + 8 * instr.reg_dest), FIRST_REG);*/
+        err |= fe_enc64(&current, FE_MOV64rm, SECOND_REG, FE_MEM_ADDR(r_info.base + 8 * instr.reg_src_1));
+        err |= fe_enc64(&current, FE_MOVZXr64m16, SECOND_REG, FE_MEM(SECOND_REG, 0, 0, instr.imm));
+        err |= fe_enc64(&current, FE_MOV64mr, FE_MEM_ADDR(r_info.base + 8 * instr.reg_dest), FIRST_REG);
     }
 }
 
@@ -115,12 +137,16 @@ void translate_LHU(const t_risc_instr &instr, const register_info &r_info) {
 void translate_SB(const t_risc_instr &instr, const register_info &r_info) {
     log_asm_out("Translate SB…\n");
 
-    if (r_info.mapped[instr.reg_src_1] && r_info.mapped[instr.reg_src_2]) {
-        a->mov(x86::ptr(r_info.map[instr.reg_src_1], instr.imm), r_info.map[instr.reg_src_2].r8());
+    if (r_info.mapped[instr.reg_src_1] || r_info.mapped[instr.reg_src_2]) {
+//        a->mov(x86::ptr(r_info.map[instr.reg_src_1], instr.imm), r_info.map[instr.reg_src_2].r8());
+        critical_not_yet_implemented("Register mapped instruction type unavailable\n");
     } else {
-        a->mov(SECOND_REG, x86::ptr(r_info.base + 8 * instr.reg_src_1));
+        /*a->mov(SECOND_REG, x86::ptr(r_info.base + 8 * instr.reg_src_1));
         a->mov(FIRST_REG, x86::ptr(r_info.base + 8 * instr.reg_src_2));
-        a->mov(x86::ptr(SECOND_REG, instr.imm), FIRST_REG.r8());
+        a->mov(x86::ptr(SECOND_REG, instr.imm), FIRST_REG.r8());*/
+        err |= fe_enc64(&current, FE_MOV64rm, SECOND_REG, FE_MEM_ADDR(r_info.base + 8 * instr.reg_src_1));
+        err |= fe_enc64(&current, FE_MOV64rm, FIRST_REG, FE_MEM_ADDR(r_info.base + 8 * instr.reg_src_2));
+        err |= fe_enc64(&current, FE_MOV8mr, FE_MEM(SECOND_REG, 0, 0, instr.imm), FIRST_REG);
     }
 }
 
@@ -134,12 +160,16 @@ void translate_SB(const t_risc_instr &instr, const register_info &r_info) {
 void translate_SH(const t_risc_instr &instr, const register_info &r_info) {
     log_asm_out("Translate SH…\n");
 
-    if (r_info.mapped[instr.reg_src_1] && r_info.mapped[instr.reg_src_2]) {
-        a->mov(x86::ptr(r_info.map[instr.reg_src_1], instr.imm), r_info.map[instr.reg_src_2].r16());
+    if (r_info.mapped[instr.reg_src_1] || r_info.mapped[instr.reg_src_2]) {
+//        a->mov(x86::ptr(r_info.map[instr.reg_src_1], instr.imm), r_info.map[instr.reg_src_2].r16());
+        critical_not_yet_implemented("Register mapped instruction type unavailable\n");
     } else {
-        a->mov(SECOND_REG, x86::ptr(r_info.base + 8 * instr.reg_src_1));
+        /*a->mov(SECOND_REG, x86::ptr(r_info.base + 8 * instr.reg_src_1));
         a->mov(FIRST_REG, x86::ptr(r_info.base + 8 * instr.reg_src_2));
-        a->mov(x86::ptr(SECOND_REG, instr.imm), FIRST_REG.r16());
+        a->mov(x86::ptr(SECOND_REG, instr.imm), FIRST_REG.r16());*/
+        err |= fe_enc64(&current, FE_MOV64rm, SECOND_REG, FE_MEM_ADDR(r_info.base + 8 * instr.reg_src_1));
+        err |= fe_enc64(&current, FE_MOV64rm, FIRST_REG, FE_MEM_ADDR(r_info.base + 8 * instr.reg_src_2));
+        err |= fe_enc64(&current, FE_MOV16mr, FE_MEM(SECOND_REG, 0, 0, instr.imm), FIRST_REG);
     }
 }
 
@@ -153,12 +183,16 @@ void translate_SH(const t_risc_instr &instr, const register_info &r_info) {
 void translate_SW(const t_risc_instr &instr, const register_info &r_info) {
     log_asm_out("Translate SW…\n");
 
-    if (r_info.mapped[instr.reg_src_1] && r_info.mapped[instr.reg_src_2]) {
-        a->mov(x86::ptr(r_info.map[instr.reg_src_1], instr.imm), r_info.map[instr.reg_src_2].r32());
+    if (r_info.mapped[instr.reg_src_1] || r_info.mapped[instr.reg_src_2]) {
+//        a->mov(x86::ptr(r_info.map[instr.reg_src_1], instr.imm), r_info.map[instr.reg_src_2].r32());
+        critical_not_yet_implemented("Register mapped instruction type unavailable\n");
     } else {
-        a->mov(SECOND_REG, x86::ptr(r_info.base + 8 * instr.reg_src_1));
+        /*a->mov(SECOND_REG, x86::ptr(r_info.base + 8 * instr.reg_src_1));
         a->mov(FIRST_REG, x86::ptr(r_info.base + 8 * instr.reg_src_2));
-        a->mov(x86::ptr(SECOND_REG, instr.imm), FIRST_REG.r32());
+        a->mov(x86::ptr(SECOND_REG, instr.imm), FIRST_REG.r32());*/
+        err |= fe_enc64(&current, FE_MOV64rm, SECOND_REG, FE_MEM_ADDR(r_info.base + 8 * instr.reg_src_1));
+        err |= fe_enc64(&current, FE_MOV64rm, FIRST_REG, FE_MEM_ADDR(r_info.base + 8 * instr.reg_src_2));
+        err |= fe_enc64(&current, FE_MOV32mr, FE_MEM(SECOND_REG, 0, 0, instr.imm), FIRST_REG);
     }
 }
 
@@ -176,11 +210,15 @@ void translate_LWU(const t_risc_instr &instr, const register_info &r_info) {
     /// r/m32. Instead you use mov r32, r/m32
 
     if (r_info.mapped[instr.reg_dest] && r_info.mapped[instr.reg_src_1]) {
-        a->mov(r_info.map[instr.reg_dest].r32(), x86::dword_ptr(r_info.map[instr.reg_src_1], instr.imm));
+//        a->mov(r_info.map[instr.reg_dest].r32(), x86::dword_ptr(r_info.map[instr.reg_src_1], instr.imm));
+        critical_not_yet_implemented("Register mapped instruction type unavailable\n");
     } else {
-        a->mov(SECOND_REG, x86::ptr(r_info.base + 8 * instr.reg_src_1));
+        /*a->mov(SECOND_REG, x86::ptr(r_info.base + 8 * instr.reg_src_1));
         a->mov(FIRST_REG.r32(), x86::dword_ptr(SECOND_REG, instr.imm));
-        a->mov(x86::ptr(r_info.base + 8 * instr.reg_dest), FIRST_REG);
+        a->mov(x86::ptr(r_info.base + 8 * instr.reg_dest), FIRST_REG);*/
+        err |= fe_enc64(&current, FE_MOV64rm, SECOND_REG, FE_MEM_ADDR(r_info.base + 8 * instr.reg_src_1));
+        err |= fe_enc64(&current, FE_MOV32rm, SECOND_REG, FE_MEM(SECOND_REG, 0, 0, instr.imm));
+        err |= fe_enc64(&current, FE_MOV64mr, FE_MEM_ADDR(r_info.base + 8 * instr.reg_dest), FIRST_REG);
     }
 }
 
@@ -194,12 +232,16 @@ void translate_LWU(const t_risc_instr &instr, const register_info &r_info) {
 void translate_LD(const t_risc_instr &instr, const register_info &r_info) {
     log_asm_out("Translate LD…\n");
 
-    if (r_info.mapped[instr.reg_dest] && r_info.mapped[instr.reg_src_1]) {
-        a->mov(r_info.map[instr.reg_dest], x86::qword_ptr(r_info.map[instr.reg_src_1], instr.imm));
+    if (r_info.mapped[instr.reg_dest] || r_info.mapped[instr.reg_src_1]) {
+//        a->mov(r_info.map[instr.reg_dest], x86::qword_ptr(r_info.map[instr.reg_src_1], instr.imm));
+        critical_not_yet_implemented("Register mapped instruction type unavailable\n");
     } else {
-        a->mov(SECOND_REG, x86::ptr(r_info.base + 8 * instr.reg_src_1));
+        /*a->mov(SECOND_REG, x86::ptr(r_info.base + 8 * instr.reg_src_1));
         a->mov(FIRST_REG, x86::qword_ptr(SECOND_REG, instr.imm));
-        a->mov(x86::ptr(r_info.base + 8 * instr.reg_dest), FIRST_REG);
+        a->mov(x86::ptr(r_info.base + 8 * instr.reg_dest), FIRST_REG);*/
+        err |= fe_enc64(&current, FE_MOV64rm, SECOND_REG, FE_MEM_ADDR(r_info.base + 8 * instr.reg_src_1));
+        err |= fe_enc64(&current, FE_MOV64rm, SECOND_REG, FE_MEM(SECOND_REG, 0, 0, instr.imm));
+        err |= fe_enc64(&current, FE_MOV64mr, FE_MEM_ADDR(r_info.base + 8 * instr.reg_dest), FIRST_REG);
     }
 }
 
@@ -213,11 +255,15 @@ void translate_LD(const t_risc_instr &instr, const register_info &r_info) {
 void translate_SD(const t_risc_instr &instr, const register_info &r_info) {
     log_asm_out("Translate SD…\n");
 
-    if (r_info.mapped[instr.reg_src_1] && r_info.mapped[instr.reg_src_2]) {
-        a->mov(x86::ptr(r_info.map[instr.reg_src_1], instr.imm), r_info.map[instr.reg_src_2]);
+    if (r_info.mapped[instr.reg_src_1] || r_info.mapped[instr.reg_src_2]) {
+//        a->mov(x86::ptr(r_info.map[instr.reg_src_1], instr.imm), r_info.map[instr.reg_src_2]);
+        critical_not_yet_implemented("Register mapped instruction type unavailable\n");
     } else {
-        a->mov(SECOND_REG, x86::ptr(r_info.base + 8 * instr.reg_src_1));
-        a->mov(FIRST_REG, x86::ptr(r_info.base + 8 * instr.reg_src_2)); //todo shouldn't this be instr.reg_src_2?
-        a->mov(x86::ptr(SECOND_REG, instr.imm), FIRST_REG);
+        /*a->mov(SECOND_REG, x86::ptr(r_info.base + 8 * instr.reg_src_1));
+        a->mov(FIRST_REG, x86::ptr(r_info.base + 8 * instr.reg_src_2));
+        a->mov(x86::ptr(SECOND_REG, instr.imm), FIRST_REG);*/
+        err |= fe_enc64(&current, FE_MOV64rm, SECOND_REG, FE_MEM_ADDR(r_info.base + 8 * instr.reg_src_1));
+        err |= fe_enc64(&current, FE_MOV64rm, FIRST_REG, FE_MEM_ADDR(r_info.base + 8 * instr.reg_src_2));
+        err |= fe_enc64(&current, FE_MOV64mr, FE_MEM(SECOND_REG, 0, 0, instr.imm), FIRST_REG);
     }
 }
