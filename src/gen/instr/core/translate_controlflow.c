e05d3c3ef6b6a32abb6f50b2355cc92271564832
Johannes Pfannschmidt <johannes.pfannschmidt@cs.tum.edu>
2020-09-07 16:06:29 +0200

    log invalid-translation

--- src/gen/instr/core/translate_controlflow.c
+++ src/gen/instr/core/translate_controlflow.c
@@ -370,6 +370,7 @@ translate_controlflow_set_pc2(const t_risc_instr *instr, const register_info *r_
 }
 
 void translate_INVALID(const t_risc_instr *instr) {
+    log_asm_out("Translate INVALID_OP\n");
     ///call error handler
     fe_enc64(&current, FE_MOV64ri, FE_DI, (uint64_t) instr->mnem);
     fe_enc64(&current, FE_MOV64ri, FE_SI, (uint64_t) instr->imm);
