0b15a8c098eab18f083cfc16c534cdc7983535f9
Johannes Pfannschmidt <johannes.pfannschmidt@cs.tum.edu>
2020-09-15 23:13:52 +0200

    update chain_end on oversized block

--- src/gen/instr/pseudo/translate_pseudo.c
+++ src/gen/instr/pseudo/translate_pseudo.c
@@ -15,6 +15,12 @@
  * @param reg_base the base_address of the gp_reg_file.
  */
 void translate_PC_NEXT_INST(const t_risc_addr addr, uint64_t reg_base) {
+    ///write chainEnd to be chained by chainer
+    if (flag_translate_opt) {
+        err |= fe_enc64(&current, FE_LEA64rm, FE_AX, FE_MEM(FE_IP, 0, 0, 0));
+        err |= fe_enc64(&current, FE_MOV64mr, FE_MEM_ADDR((uint64_t) &chain_end), FE_AX);
+    }
+
     ///set pc
     err |= fe_enc64(&current, FE_MOV64ri, FE_AX, addr);
     err |= fe_enc64(&current, FE_MOV64mr, FE_MEM_ADDR(reg_base + 8 * pc), FE_AX);
