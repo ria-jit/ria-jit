126d8077f2b6bde8844247111c72985f03a97f76
noah <n.dormann@tum.de>
2020-10-02 09:05:47 +0200

    Added test for f and d extension artihm translations

---
+++ test/unit_tests/test_double_arithm.cpp
@@ -0,0 +1,320 @@
+//
+// Created by simon on 17.09.20.
+//
+
+#include <gtest/gtest.h>
+#include <util/typedefs.h>
+#include <main/context.h>
+#include <gen/translate.h>
+#include <runtime/register.h>
+#include "math.h"
+
+#define attr_unused __attribute__((__unused__))
+
+typedef double (*t_arithmResFunc)(double, double);
+
+inline t_risc_fp_reg_val getdouble(double d) {
+    t_risc_fp_reg_val ret;
+    ret.d = d;
+    return ret;
+}
+
+/**
+ * Parameterized using the following parameters:
+ *
+ * @param t_risc_mnem the mnemonic of the Arithm Instruction.
+ * @param t_risc_reg_val the initial value for rs1
+ * @param t_risc_reg_val the initial value for rs2
+ * @param t_res_func the functor/lambda that provides, given the addr of the instruction, the value of rs1 and the value
+ *        in rs2
+ * @param bool whether rs1 should be mapped. (if multiple registers are the same for a test the priority is rs1>rs2>rd.)
+ * @param bool whether rs2 should be mapped. (if multiple registers are the same for a test the priority is rs1>rs2>rd.)
+ * @param bool whether rd should be mapped. (if multiple registers are the same for a test the priority is rs1>rs2>rd.)
+ */
+class ArithmDoubleTest :
+        public ::testing::TestWithParam<
+                std::tuple<t_risc_mnem, double, double, t_arithmResFunc, bool, bool,
+                        bool>> {
+protected:
+    t_risc_mnem mnem{};
+    double rs1StartValue{};
+    double rs2StartValue{};
+    t_arithmResFunc resFunc{};
+    double expectedRd;
+    bool rs1Mapped{};
+    bool rs2Mapped{};
+    bool rdMapped{};
+
+    t_risc_reg rs1 = x0;
+    t_risc_reg rs2 = x0;
+    t_risc_reg rd = x0;
+
+    t_risc_instr blockCache[1]{};
+    static context_info *c_info;
+    static register_info *r_info;
+
+protected:
+    ArithmDoubleTest() {
+        std::tie(mnem, rs1StartValue, rs2StartValue, resFunc, rs1Mapped, rs2Mapped,
+                 rdMapped) =
+                GetParam();
+
+        expectedRd = resFunc(rs1StartValue, rs2StartValue);
+    }
+
+public:
+
+    static void SetUpTestSuite() {
+        if (c_info == nullptr) {
+            c_info = init_map_context();
+            r_info = c_info->r_info;
+        }
+    }
+
+protected:
+
+    void SetUp() override {
+        for (int i = 1; i < N_REG; ++i) {
+            bool curMap = r_info->mapped[i];
+            if (rs1 == x0 && curMap == rs1Mapped) {
+                rs1 = static_cast<t_risc_reg>(i);
+            } else if (rs2 == x0 && curMap == rs2Mapped) {
+                rs2 = static_cast<t_risc_reg>(i);
+            } else if (rd == x0 && curMap == rdMapped) {
+                rd = static_cast<t_risc_reg>(i);
+            }
+        }
+
+    }
+};
+
+context_info *ArithmDoubleTest::c_info = nullptr;
+register_info *ArithmDoubleTest::r_info = nullptr;
+
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunknown-pragmas"
+//This should ignore the "unknown" ide pragmas without error, but the diagnostic pragmas in GCC don't work on stuff only
+// the preprocessor sees.
+#pragma ide diagnostic ignored "cppcoreguidelines-avoid-non-const-global-variables"
+#pragma ide diagnostic ignored "cert-err58-cpp"
+
+TEST_P(ArithmDoubleTest, AllDifferent) {
+    blockCache[0] =
+            t_risc_instr{0, mnem, static_cast<t_risc_optype>(0), rs1, rs2, rd, 0};
+
+    t_cache_loc loc = translate_block_instructions(blockCache, 1, c_info);
+
+    set_fpvalue(rs1, getdouble(rs1StartValue));
+    set_fpvalue(rs2, getdouble(rs2StartValue));
+
+    execute_in_guest_context(c_info, loc);
+
+    EXPECT_EQ(rs1StartValue, get_fpvalue(rs1).d);
+    EXPECT_EQ(rs2StartValue, get_fpvalue(rs2).d);
+    EXPECT_EQ(expectedRd, get_fpvalue(rd).d);
+}
+
+TEST_P(ArithmDoubleTest, Rs1RdSame) {
+    blockCache[0] =
+            t_risc_instr{0, mnem, static_cast<t_risc_optype>(0), rs1, rs2, rs1, 0};
+
+    t_cache_loc loc = translate_block_instructions(blockCache, 1, c_info);
+
+    set_fpvalue(rs1, getdouble(rs1StartValue));
+    set_fpvalue(rs2, getdouble(rs2StartValue));
+
+    execute_in_guest_context(c_info, loc);
+
+    EXPECT_EQ(expectedRd, get_fpvalue(rs1).d);
+    EXPECT_EQ(rs2StartValue, get_fpvalue(rs2).d);
+}
+
+TEST_P(ArithmDoubleTest, Rs2RdSame) {
+        blockCache[0] =
+                t_risc_instr{0, mnem, static_cast<t_risc_optype>(0), rs1, rs2, rs2, 0};
+
+        t_cache_loc loc = translate_block_instructions(blockCache, 1, c_info);
+
+        set_fpvalue(rs1, getdouble(rs1StartValue));
+        set_fpvalue(rs2, getdouble(rs2StartValue));
+
+        execute_in_guest_context(c_info, loc);
+
+        EXPECT_EQ(rs1StartValue, get_fpvalue(rs1).d);
+        EXPECT_EQ(expectedRd, get_fpvalue(rs2).d);
+}
+
+TEST_P(ArithmDoubleTest, Rs1Rs2Same) {
+        blockCache[0] = t_risc_instr{0, mnem, static_cast<t_risc_optype>(0), rs1, rs1, rd, 0};
+
+        t_cache_loc loc = translate_block_instructions(blockCache, 1, c_info);
+
+        set_fpvalue(rs1, getdouble(rs1StartValue));
+
+        execute_in_guest_context(c_info, loc);
+
+        EXPECT_EQ(rs1StartValue, get_fpvalue(rs1).d);
+        EXPECT_EQ(resFunc(rs1StartValue, rs1StartValue), get_fpvalue(rd).d);
+}
+
+TEST_P(ArithmDoubleTest, AllSame) {
+        blockCache[0] = t_risc_instr{0, mnem, static_cast<t_risc_optype>(0), rs1, rs1, rs1, 0};
+
+        t_cache_loc loc = translate_block_instructions(blockCache, 1, c_info);
+
+        set_fpvalue(rs1, getdouble(rs1StartValue));
+
+        execute_in_guest_context(c_info, loc);
+
+        EXPECT_EQ(resFunc(rs1StartValue, rs1StartValue), get_fpvalue(rs1).d);
+}
+
+INSTANTIATE_TEST_SUITE_P(FADDD,
+                         ArithmDoubleTest,
+                         testing::Combine(
+                                 testing::Values(FADDD),
+                                 testing::Values(1, -1),
+                                 testing::Values(2, -5, 1),
+                                 testing::Values([](double rs1,double rs2) {
+                                     return rs1 + rs2;
+                                 }),
+                                 testing::Values(false),
+                                 testing::Values(false),
+                                 testing::Values(false)));
+
+INSTANTIATE_TEST_SUITE_P(FSUBD,
+                         ArithmDoubleTest,
+                         testing::Combine(
+                                 testing::Values(FSUBD),
+                                 testing::Values(1, -1),
+                                 testing::Values(2, -5, 1),
+                                 testing::Values([](double rs1,double rs2) {
+                                     return rs1 - rs2;
+                                 }),
+                                 testing::Values(false),
+                                 testing::Values(false),
+                                 testing::Values(false)));
+
+INSTANTIATE_TEST_SUITE_P(FMULD,
+                         ArithmDoubleTest,
+                         testing::Combine(
+                                 testing::Values(FMULD),
+                                 testing::Values(1, -1),
+                                 testing::Values(2, -5, 1),
+                                 testing::Values([](double rs1,double rs2) {
+                                     return rs1 * rs2;
+                                 }),
+                                 testing::Values(false),
+                                 testing::Values(false),
+                                 testing::Values(false)));
+
+INSTANTIATE_TEST_SUITE_P(FDIVD,
+                         ArithmDoubleTest,
+                         testing::Combine(
+                                 testing::Values(FDIVD),
+                                 testing::Values(1, -1),
+                                 testing::Values(2, -5, 1),
+                                 testing::Values([](double rs1,double rs2) {
+                                     return rs1 / rs2;
+                                 }),
+                                 testing::Values(false),
+                                 testing::Values(false),
+                                 testing::Values(false)));
+
+INSTANTIATE_TEST_SUITE_P(FSQRTD,
+                         ArithmDoubleTest,
+                         testing::Combine(
+                                 testing::Values(FSQRTD),
+                                 testing::Values(1, 5,64,2601),
+                                 testing::Values(0),
+                                 testing::Values([](double rs1,double rs2 attr_unused) {
+                                     return sqrt(rs1);
+                                 }),
+                                 testing::Values(false),
+                                 testing::Values(false),
+                                 testing::Values(false)));
+
+
+INSTANTIATE_TEST_SUITE_P(FSGNJD,
+                         ArithmDoubleTest,
+                         testing::Combine(
+                                 testing::Values(FSGNJD),
+                                 testing::Values(1,-1),
+                                 testing::Values(-5,5),
+                                 testing::Values([](double rs1,double rs2) {
+                                     //get signs
+                                     if((rs2 >= 0 && rs1 < 0) || (rs2 < 0 && rs1 >= 0)){
+                                         return -rs1;
+                                     }
+                                     return rs1;
+                                 }),
+                                 testing::Values(false),
+                                 testing::Values(false),
+                                 testing::Values(false)));
+
+
+INSTANTIATE_TEST_SUITE_P(FSGNJND,
+                         ArithmDoubleTest,
+                         testing::Combine(
+                                 testing::Values(FSGNJND),
+                                 testing::Values(1,-1),
+                                 testing::Values(-5,5),
+                                 testing::Values([](double rs1,double rs2) {
+                                     //get signs
+                                     if((rs2 >= 0 && rs1 >= 0) || (rs2 < 0 && rs1 < 0)){
+                                         return -rs1;
+                                     }
+                                     return rs1;
+                                 }),
+                                 testing::Values(false),
+                                 testing::Values(false),
+                                 testing::Values(false)));
+
+INSTANTIATE_TEST_SUITE_P(FSGNJXD,
+                         ArithmDoubleTest,
+                         testing::Combine(
+                                 testing::Values(FSGNJXD),
+                                 testing::Values(1,-1),
+                                 testing::Values(-5,5),
+                                 testing::Values([](double rs1,double rs2) {
+                                     //get signs
+                                     if((rs2 < 0 && rs1 >= 0) || (rs2 < 0 && rs1 < 0)){
+                                         return -rs1;
+                                     }
+                                     return rs1;
+                                 }),
+                                 testing::Values(false),
+                                 testing::Values(false),
+                                 testing::Values(false)));
+
+INSTANTIATE_TEST_SUITE_P(FMIND,
+                         ArithmDoubleTest,
+                         testing::Combine(
+                                 testing::Values(FMIND),
+                                 testing::Values(1,-1),
+                                 testing::Values(-5,5),
+                                 testing::Values([](double rs1,double rs2) {
+                                     //return smaller
+                                     return rs1 < rs2 ? rs1 : rs2;
+                                 }),
+                                 testing::Values(false),
+                                 testing::Values(false),
+                                 testing::Values(false)));
+
+INSTANTIATE_TEST_SUITE_P(FMAXD,
+                         ArithmDoubleTest,
+                         testing::Combine(
+                                 testing::Values(FMAXD),
+                                 testing::Values(1,-1),
+                                 testing::Values(-5,5),
+                                 testing::Values([](double rs1,double rs2) {
+                                     //return bigger
+                                     return rs1 > rs2 ? rs1 : rs2;
+                                 }),
+                                 testing::Values(false),
+                                 testing::Values(false),
+                                 testing::Values(false)));
+
+
+#pragma ide diagonstics pop
+#pragma GCC diagnostic pop
