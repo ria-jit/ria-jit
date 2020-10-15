31a339a3bab7ba7330856f2998b068fdcb216660
Simon Kammemeier <simon.kammermeier@tum.de>
2020-10-15 18:52:10 +0200

    Fix PC_NEXT_INST not invalidating RAX before use.

--- src/gen/instr/pseudo/translate_pseudo.c
+++ src/gen/instr/pseudo/translate_pseudo.c
@@ -3,6 +3,7 @@
 //
 
 #include <gen/translate.h>
+#include <util/util.h>
 #include "translate_pseudo.h"
 
 ///set the pc to next addr after inst
@@ -11,10 +12,12 @@
  * Emits instructions that sets the PC RISC_V register in our gp_reg_file to the given address.
  * @param addr the address the risc PC reg should have at the end of the current block (the start address of the next
  *        block)
- * @param reg_base the base_address of the gp_reg_file.
+ * @param r_info the runtime register mapping (RISC-V -> x86)
  */
-void translate_PC_NEXT_INST(const t_risc_addr addr, uint64_t reg_base) {
+void translate_PC_NEXT_INST(const t_risc_addr addr, const register_info *r_info) {
+    log_asm_out("Translate pseudo PC_NEXT_INST");
     ///write chainEnd to be chained by chainer
+    invalidateReplacement(r_info, FE_AX, true);
     if (flag_translate_opt_chain) {
         err |= fe_enc64(&current, FE_LEA64rm, FE_AX, FE_MEM(FE_IP, 0, 0, 0));
         err |= fe_enc64(&current, FE_MOV64mr, FE_MEM_ADDR((uint64_t) &chain_end), FE_AX);
@@ -22,7 +25,7 @@ void translate_PC_NEXT_INST(const t_risc_addr addr, uint64_t reg_base) {
 
     ///set pc
     err |= fe_enc64(&current, FE_MOV64ri, FE_AX, addr);
-    err |= fe_enc64(&current, FE_MOV64mr, FE_MEM_ADDR(reg_base + 8 * pc), FE_AX);
+    err |= fe_enc64(&current, FE_MOV64mr, FE_MEM_ADDR(r_info->base + 8 * pc), FE_AX);
 }
 
 /**
