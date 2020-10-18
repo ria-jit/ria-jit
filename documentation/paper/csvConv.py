ade1a0e3a05273ce3a9d5a5be60e365fe9545938
Simon Kammemeier <simon.kammermeier@tum.de>
2020-10-18 16:41:45 +0200

    Adapt to load benchmark results from CSVs and normalize them.
    The CSVs need to be "prepared" first. For this you should call
    csvConv.py <inputFile> <outputFile (native.csv, qemu.csv or dbt.csv)>

---
+++ documentation/paper/csvConv.py
@@ -0,0 +1,21 @@
+import csv
+import sys
+
+
+def escape(string):
+    return '{' + string.replace('{', "\\{").replace("}", "\\}").replace("#", "No.").replace("_s", "") + '}'
+
+
+def main():
+    in_file = open(sys.argv[1], "r")
+    out_file = open(sys.argv[2], "w")
+    csv_file = csv.reader(in_file)
+    line_count = 0
+    for row in csv_file:
+        line_count += 1
+        if 7 <= line_count < 18:
+            out_file.write(",".join(map(escape, row)) + "\n")
+
+
+if __name__ == '__main__':
+    main()
