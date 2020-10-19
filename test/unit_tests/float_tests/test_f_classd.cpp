e1c91639be18b5c169c9d6791b2e017c0b93f4e0
noah <n.dormann@tum.de>
2020-10-19 13:50:03 +0200

    Added test for FCLASSD

--- test/unit_tests/float_tests/test_f_classd.cpp
+++ test/unit_tests/float_tests/test_f_classd.cpp
@@ -12,7 +12,7 @@
  * Checks the load and store translation by copying data from an array into the registers and back into another array
  */
 namespace {
-    class FClassTest : public ::testing::Test {
+    class FClassDTest : public ::testing::Test {
     protected:
         t_risc_reg src = x1;
         t_risc_reg rd = x1;
@@ -21,41 +21,41 @@ namespace {
         context_info *c_info;
         t_cache_loc loc;
 
-        FClassTest() {
+        FClassDTest() {
 
         }
 
-        virtual ~FClassTest() {
+        virtual ~FClassDTest() {
 
         }
 
         virtual void SetUp() {
             blockCache[0] =
-                    t_risc_instr{0, FCLASSS, static_cast<t_risc_optype>(0), src, src, rd, 0};
+                    t_risc_instr{0, FCLASSD, static_cast<t_risc_optype>(0), src, src, rd, 0};
 
             c_info = init_map_context();
             loc = translate_block_instructions(blockCache, 1, c_info);
         }
     };
 
-    TEST_F(FClassTest, NegativeInfinity) {
-        uint32_t testVal = 0xff800000;
+    TEST_F(FClassDTest, NegativeInfinity) {
+        uint64_t testVal = 0xfff0000000000000;
         set_fpvalue(src, get_iVal(testVal));
         execute_in_guest_context(c_info, loc);
         EXPECT_EQ(testVal, get_fpvalue(src).i);
         EXPECT_EQ((1 << 0), get_value(rd));
     }
 
-    TEST_F(FClassTest, NegativeNormal) {
-        float testVal = -100.0;
-        set_fpvalue(src, get_fVal(testVal));
+    TEST_F(FClassDTest, NegativeNormal) {
+        double testVal = -10000.0;
+        set_fpvalue(src, get_dVal(testVal));
         execute_in_guest_context(c_info, loc);
-        EXPECT_EQ(testVal, get_fpvalue(src).f);
+        EXPECT_EQ(testVal, get_fpvalue(src).d);
         EXPECT_EQ((1 << 1), get_value(rd));
     }
 
-    TEST_F(FClassTest, NegativeSubNormal) {
-        uint32_t testVal = 0x800000ff;
+    TEST_F(FClassDTest, NegativeSubNormal) {
+        uint64_t testVal = 0x80000000000000ff;
         set_fpvalue(src, get_iVal(testVal));
         execute_in_guest_context(c_info, loc);
         EXPECT_EQ(testVal, get_fpvalue(src).i);
@@ -63,16 +63,16 @@ namespace {
     }
 
 
-    TEST_F(FClassTest, NegativeZero) {
-        uint32_t testVal = 0x80000000;
+    TEST_F(FClassDTest, NegativeZero) {
+        uint64_t testVal = 0x8000000000000000;
         set_fpvalue(src, get_iVal(testVal));
         execute_in_guest_context(c_info, loc);
         EXPECT_EQ(testVal, get_fpvalue(src).i);
         EXPECT_EQ((1 << 3), get_value(rd));
     }
 
-    TEST_F(FClassTest, PositiveZero) {
-        uint32_t testVal = 0x0;
+    TEST_F(FClassDTest, PositiveZero) {
+        uint64_t testVal = 0x0;
         set_fpvalue(src, get_iVal(testVal));
         execute_in_guest_context(c_info, loc);
         EXPECT_EQ(testVal, get_fpvalue(src).i);
@@ -80,8 +80,8 @@ namespace {
     }
 
 
-    TEST_F(FClassTest, PositiveSubNormal) {
-        uint32_t testVal = 0x000000ff;
+    TEST_F(FClassDTest, PositiveSubNormal) {
+        uint64_t testVal = 0x00000000000000ff;
         set_fpvalue(src, get_iVal(testVal));
         execute_in_guest_context(c_info, loc);
         EXPECT_EQ(testVal, get_fpvalue(src).i);
@@ -89,32 +89,32 @@ namespace {
     }
 
 
-    TEST_F(FClassTest, PositiveNormal) {
-        float testVal = 100.0;
-        set_fpvalue(src, get_fVal(testVal));
+    TEST_F(FClassDTest, PositiveNormal) {
+        double testVal = 100.0;
+        set_fpvalue(src, get_dVal(testVal));
         execute_in_guest_context(c_info, loc);
-        EXPECT_EQ(testVal, get_fpvalue(src).f);
+        EXPECT_EQ(testVal, get_fpvalue(src).d);
         EXPECT_EQ((1 << 6), get_value(rd));
     }
 
-    TEST_F(FClassTest, PositiveInfinity) {
-        uint32_t testVal = 0x7f800000;
+    TEST_F(FClassDTest, PositiveInfinity) {
+        uint64_t testVal = 0x7ff0000000000000;
         set_fpvalue(src, get_iVal(testVal));
         execute_in_guest_context(c_info, loc);
         EXPECT_EQ(testVal, get_fpvalue(src).i);
         EXPECT_EQ((1 << 7), get_value(rd));
     }
 
-    TEST_F(FClassTest, SignalingNAN) {
-        uint32_t testVal = 0x7f800001;
+    TEST_F(FClassDTest, SignalingNAN) {
+        uint64_t testVal = 0x7ff0000000000001;
         set_fpvalue(src, get_iVal(testVal));
         execute_in_guest_context(c_info, loc);
         EXPECT_EQ(testVal, get_fpvalue(src).i);
         EXPECT_EQ((1 << 8), get_value(rd));
     }
 
-    TEST_F(FClassTest, SilentNAN) {
-        uint32_t testVal = 0x7fc00000;
+    TEST_F(FClassDTest, SilentNAN) {
+        uint64_t testVal = 0x7ff8000000000000;
         set_fpvalue(src, get_iVal(testVal));
         execute_in_guest_context(c_info, loc);
         EXPECT_EQ(testVal, get_fpvalue(src).i);
